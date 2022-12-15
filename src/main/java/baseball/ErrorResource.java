ede6b2318a6e43af682ccd2c3eb79c27898b8f95
starwook <star.wookk@gmail.com>
2022-12-15 13:59:26 +0900

    feat(Add ErrorResource.java):

---
+++ src/main/java/baseball/ErrorResource.java
@@ -0,0 +1,10 @@
+package baseball;
+
+public class ErrorResource {
+    public static String START ="[ERROR] ";
+    public static String NOT_NUMBER = "숫자를 입력하셔야 합니다";
+    public static String NOT_SIZE = "숫자의 사이즈가 잘못되었습니다";
+    public static String DUPLICATE_NUMBER = "중복된 숫자가 존재합니다";
+    public static String NOT_IN_RANGE = "범위 바깥의 숫자가 존재합니다";
+
+}
