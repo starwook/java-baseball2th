4af8cadc584d86772655a6bcb1f93a181891e549
starwook <star.wookk@gmail.com>
2022-12-15 14:19:57 +0900

    feat: Add checkIfDuplicate method

--- src/main/java/baseball/BaseBallJudgeMent.java
+++ src/main/java/baseball/BaseBallJudgeMent.java
@@ -11,21 +11,30 @@ public class BaseBallJudgeMent {
 
     public void getResult(BaseBallNumber baseBallNumber,String input){
         checkException(baseBallNumber.getNumberSize(),input);
-        createInputNumber(input);
+
         calculate();
     }
     public void checkException(int numberSize, String input){
         checkInputSize(numberSize, input);
         checkNumberInRange(input);
         checkInputIsNumber(input);
+        CreateInputNumber(input);
     }
     public void calculate(){
 
     }
 
-    private void createInputNumber(String input) {
+    private void CreateInputNumber(String input) {
         for(int i = 0; i< input.length(); i++){
+            int newNumber = input.charAt(i)-'0';
+            checkIfNumberDuplicate(newNumber);
             inputNumbers.add(input.charAt(i)-'0');
+        }
+    }
+
+    private void checkIfNumberDuplicate(int newNumber) {
+        if(inputNumbers.contains(newNumber)){
+            throw new IllegalArgumentException(ErrorResource.START+ErrorResource.DUPLICATE_NUMBER);
         }
     }
 
