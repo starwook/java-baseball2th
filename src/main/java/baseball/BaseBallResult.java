e00e8413c9a8b291352ca5dc5cb4aba7e64b92e3
starwook <star.wookk@gmail.com>
2022-12-15 13:39:48 +0900

    feat: Add getter method

--- src/main/java/baseball/BaseBallResult.java
+++ src/main/java/baseball/BaseBallResult.java
@@ -1,11 +1,17 @@
 package baseball;
 
 public class BaseBallResult {
+    private static int STANDARD_SUCCESS_NUMBER =3;
     private int strike;
     private int ball;
     public BaseBallResult(){
         strike =0;
         ball =0;
     }
-
+    public int getBall(){
+        return ball;
+    }
+    public int getStrike(){
+        return strike;
+    }
 }
